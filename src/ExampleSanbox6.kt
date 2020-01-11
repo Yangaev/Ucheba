d68eb3dfbfd8f6aa10c06c3820dd292df7c4bb59
Andrey <apakalipsis092@gmail.com>
2020-01-12 02:37:39 +0700

    ExampleSandbox5_inline

--- src/ExampleSanbox6.kt
+++ src/ExampleSanbox6.kt
@@ -3,14 +3,18 @@
 Добавляем к предыдушему примеру еще одну функцию runSimulation
  */
 fun main() {
-    val greetingFuction = {playerName:String, numBuildings:Int ->
+//    val greetingFuction = {playerName:String, numBuildings:Int ->
+//    Используем сокращенный синтаксис
+      runSimulation("Жора"){playerName, numBuildings ->
         val currentYear = 2018
         println("Добавили $numBuildings дома")
         "Добро пожаловать в СимВиллаж, $playerName!($currentYear)"
     }
-    runSimulation("Жора",greetingFuction)
+//    runSimulation("Жора",greetingFuction)
 }
-fun runSimulation(playerName:String, greetingFunction: (String,Int) ->String){
+//Используем встривание
+
+ inline fun runSimulation(playerName:String, greetingFunction: (String,Int) ->String){
     val numBuildings = (1..3).shuffled().last() //Здесь случайным образом выбираем число от 1 до 3
     println(greetingFunction(playerName,numBuildings))
 }
