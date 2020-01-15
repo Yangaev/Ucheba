62b22fdd851933c91bd6138d85d060bbe3b7f3b9
Andrey <apakalipsis092@gmail.com>
2020-01-15 23:00:11 +0700

    ExampleSandbox6_7_function-function

--- src/ExampleSanbox6.kt
+++ src/ExampleSanbox6.kt
@@ -1,20 +1,25 @@
 /*
 Функция, которая принимает функцию
 Добавляем к предыдушему примеру еще одну функцию runSimulation
+Ссылка на функцию
+Объявление функции printConstructionCost
  */
 fun main() {
-//    val greetingFuction = {playerName:String, numBuildings:Int ->
 //    Используем сокращенный синтаксис
-      runSimulation("Жора"){playerName, numBuildings ->
+      runSimulations("Жора",::printConstructionCost){playerName, numBuildings ->
         val currentYear = 2018
         println("Добавили $numBuildings дома")
         "Добро пожаловать в СимВиллаж, $playerName!($currentYear)"
     }
-//    runSimulation("Жора",greetingFuction)
 }
 //Используем встривание
-
- inline fun runSimulation(playerName:String, greetingFunction: (String,Int) ->String){
+ inline fun runSimulations(playerName:String, costPrinter :(Int)->Unit,
+                           greetingFunction: (String,Int) ->String){
     val numBuildings = (1..3).shuffled().last() //Здесь случайным образом выбираем число от 1 до 3
+    costPrinter(numBuildings)
     println(greetingFunction(playerName,numBuildings))
+}
+fun printConstructionCost (numBuildings:Int){
+    val cost = 500
+    println("стоимость строительства: ${cost * numBuildings}")
 }
---
+++ src/ExampleSandbox7.kt
@@ -0,0 +1,26 @@
+/*
+Тип функции как возвращаемый тип
+Добавляем функцию configureGreetingFunction
+ */
+fun main() {
+    runSimulation()
+}
+//Используем встривание
+
+ fun runSimulation(){
+    val greetingFunction = configureGreetingFunction()
+     println(greetingFunction("Жора"))
+
+}
+//Функция configureGreetingFunction
+fun configureGreetingFunction():(String)-> String{
+    val structureType = "госпиталь"
+    var numBuildings = 5
+    return {
+        playerName : String ->
+        val currentYaer = 2018
+        numBuildings += 1
+        println("Добавленно $numBuildings $structureType")
+        "Добро пожаловать в СимВилаж, $playerName!(c $currentYaer)"
+    }
+}
