74c9465ca76ef73fefeca7b9c2a1b373f2a3832c
Andrey <apakalipsis092@gmail.com>
2020-02-15 01:34:05 +0700

    ExampleNyetHackTavern7_String_split

--- src/ExampleNyetHacj=kTavern7.kt
+++ src/ExampleNyetHacj=kTavern7.kt
@@ -7,4 +7,9 @@ private fun placeOrder(menuData:String){
     val indexOfApostraphe = TAVERN.indexOf('\'')
     val tavernMasters = TAVERN.substring(0 until indexOfApostraphe)
     println("Мадригал рассказывает $tavernMasters о своем заказе")
+/* Добавляем функцию split - которая разобьет нашу строку на подстроки */
+    val (type, name, price) = menuData.split(',')
+    val message = "Мадригал покупает $name ($type), по цене $price"
+    println(message)
+
 }
