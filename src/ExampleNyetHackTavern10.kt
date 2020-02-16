4903a5ff14f3fb442e3d9470458b2164af957791
Andrey <apakalipsis092@gmail.com>
2020-02-16 21:28:29 +0700

    ExampleNyetHackTavern10_Int

---
+++ src/ExampleNyetHackTavern10.kt
@@ -0,0 +1,43 @@
+//Настройка кошелька игрока
+const val TAVEREN_NAME = "Billbo's Begins"
+//Переменные для хранения золотых монет
+val playerGold = 10
+val playerSilver = 10
+fun main() {
+    placeOrder("shandy,Dragon's Breath, 5.91")
+}
+
+private fun placeOrder(menuData:String){
+val indexOfAppostraphe = TAVEREN_NAME.indexOf('\'')
+    val tavernMaster = TAVEREN_NAME.substring(0 until indexOfAppostraphe)
+    val (type,name,price) = menuData.split(',')
+    val message = "Мадригал покупает у $tavernMaster: $name ($type) по цене $price"
+    println("Мадригал разговаривает с $tavernMaster о своём заказе")
+    println(message)
+    val phrase = if(name =="Dragon's Breath"){
+        "Мадригал восклицает ${toDragonSpeak("Ах, какой вкусный $name ")}"
+    }else {
+        "Мадригал говорть: Спасибо за $name"
+    }
+    println(phrase)
+    performPurchase()
+}
+private fun toDragonSpeak(phrase:String) =
+    phrase.replace(Regex("""[а-я]|[А-Я]""")){
+        when (it.value){
+            "А" -> "0"
+            "а" ->"4"
+            "х" -> "3"
+            "к" -> "1"
+            "о" -> "|_|"
+            else -> it.value
+        }
+    }
+//Функция вывода баланса на экран
+private fun displyBalance(){
+    println("Баланс кошелька игрока:Gold: $playerGold, Silver:$playerSilver")
+}
+//Функция для вызова функции
+fun performPurchase(){
+    displyBalance()
+}
