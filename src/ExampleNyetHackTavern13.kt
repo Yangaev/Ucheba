e1b87e6d00e646628c79a513793911a34c6ec28d
Andrey <apakalipsis092@gmail.com>
2020-03-05 19:24:16 +0700

    ExampleNyetHackTaver13_List_forEachIndexed2

---
+++ src/ExampleNyetHackTavern13.kt
@@ -0,0 +1,67 @@
+//Моделирование нескольких заказов
+
+//Импортируем библиотеку
+import kotlin.math.roundToInt
+
+//
+fun main() {
+
+//    Используем forEachIndexed
+    patronListss.forEachIndexed { index, patron ->
+        println("Добрый вечер, $patron - вы #${index+1} в очреди")
+        placeOrder(patron,"shandy,Dragon's Breath, 5.91")
+    }
+//    выводим список
+    println("Вывод списка посетителей: $patronListss")
+
+
+}
+
+private fun placeOrder(patronName:String,menuData:String){
+    val indexOfAppostraphe = TAVEREN_NAME.indexOf('\'')
+    val tavernMaster = TAVEREN_NAME.substring(0 until indexOfAppostraphe)
+    val (type,name,price) = menuData.split(',')
+    val message = "$patronName покупает у $tavernMaster: $name ($type) по цене $price"
+    println("$patronName разговаривает с $tavernMaster о своём заказе")
+    println(message)
+    val phrase = if(name =="Dragon's Breath"){
+        "$patronName восклицает ${toDragonSpeak("Ах, какой вкусный $name ")}"
+    }else {
+        "$patronName говорть: Спасибо за $name"
+    }
+    println(phrase)
+    //Преобразование строки в Double
+    performPurchas(price.toDouble())
+}
+private fun toDragonSpeak(phrase:String) =
+    phrase.replace(Regex("""[а-я]|[А-Я]""")){
+        when (it.value){
+            "А" -> "0"
+            "а" ->"4"
+            "х" -> "3"
+            "к" -> "1"
+            "о" -> "|_|"
+            else -> it.value
+        }
+    }
+//Функция вывода баланса на экран
+private fun displyBalance(){
+    println("Баланс кошелька игрока:Gold: $playerGold, Silver:$playerSilver")
+}
+//Функция для вызова функции
+fun performPurchas(price:Double){
+    displyBalance()
+    val totalPurse = playerGold+(playerSilver/100.0)
+    val remainningBalance = totalPurse-price
+//    Перевод серебра в золото
+    val remainingGold = remainningBalance.toInt()
+    val remainingSilver = (remainningBalance % 1 * 100).roundToInt()
+    playerGold=remainingGold
+    playerSilver=remainingSilver
+    displyBalance()
+    println("Всего в кошельке:$totalPurse")
+    println("Покупка товара по цене: $price")
+//    Выводим остаток на балансес помощью форматирования
+    println("Остаток на балансе:${"%.2f".format(remainningBalance)}")
+}
+
